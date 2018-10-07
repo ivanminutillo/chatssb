cb0a65490382479f0f6b8096f566610c17d042a7
ivan minutillo <minutillo.ivan@gmail.com>
2018-10-07 10:04:16 +0200

    added fake channels on sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -17,31 +17,40 @@ const Wrapper = styled.View`
     `};
 `;
 
-const Header = styled.View`
-  height: 50px;
-  display: flex;
-  align-items: center;
-`;
-
-const Span = styled.View`
-  vertical-align: middle;
-  cursor: pointer;
-  flex: 1;
-  text-align: center;
-  margin-right: 8px;
+const Title = styled.Text`
+  font-size: ${props => props.theme.fontSize.h3};
+  color: ${props => props.theme.colors.p300};
+  margin-top: 16px;
   margin-left: 8px;
+  font-weight: 500;
+  letter-spacing: 0.5px;
 `;
 
-const SpanInput = styled.View`
-  flex: 10;
-  margin-right: 8px;
-  & input {
-    background: #393f50;
-    color: #f0f0f0;
-  }
+const Channel = styled.TouchableOpacity`
+  background: #29292a;
+  height: 30px;
+  padding-right: 8px;
+  padding-left: 8px;
+  margin-top: 8px;
+  border-top-left-radius: ${props => (props.active ? "0px" : "3px")};
+  border-bottom-left-radius: ${props => (props.active ? "0px" : "3px")};
+  border-left-width: ${props => (props.active ? "2px" : 0)};
+  border-left-color: ${props =>
+    props.active ? props.theme.colors.b100 : "transparent"};
+`;
+
+const ChannelTitle = styled.Text`
+  line-height: 30px;
+  letter-spacing: 0.5px;
+  font-size: ${props => props.theme.fontSize.h3};
+  color: ${props =>
+    props.active ? props.theme.colors.p100 : props.theme.colors.p200};
+  font-weight: ${props => (props.active ? "700" : "400")};
 `;
 
 const List = styled.View`
+  flex-direction: column;
+  padding: 0 8px;
   & a {
     display: block;
     &:hover {
@@ -61,8 +70,9 @@ const Sidebar = ({ togglePanel, agents, data, isOpen, toggleSidebar }) => {
           <Input placeholder="Search" />
         </SpanInput>
       </Header> */}
+      <Title>Active channels</Title>
       <List>
-        <AvatarWrapper>
+        {/* <AvatarWrapper>
           <Img source={{ uri: `https://picsum.photos/200` }} />
           <AvatarTitle>ivanminutillo</AvatarTitle>
         </AvatarWrapper>
@@ -71,7 +81,28 @@ const Sidebar = ({ togglePanel, agents, data, isOpen, toggleSidebar }) => {
             <Img src={`${a.object.image}`} />
             <AvatarTitle>{a.object.name}</AvatarTitle>
           </AvatarWrapper>
-        ))}
+        ))} */}
+        <Channel>
+          <ChannelTitle>#sentence-structure</ChannelTitle>
+        </Channel>
+        <Channel>
+          <ChannelTitle>#p2p</ChannelTitle>
+        </Channel>
+        <Channel>
+          <ChannelTitle>#feminism</ChannelTitle>
+        </Channel>
+        <Channel active>
+          <ChannelTitle active>#general</ChannelTitle>
+        </Channel>
+        <Channel>
+          <ChannelTitle>#decentralization</ChannelTitle>
+        </Channel>
+        <Channel>
+          <ChannelTitle>#smart-house</ChannelTitle>
+        </Channel>
+        <Channel>
+          <ChannelTitle>#kids</ChannelTitle>
+        </Channel>
       </List>
     </Wrapper>
   );
